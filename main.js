763da6647dbe2d45a7bc5b49f5022271c1df3549
yanivlav <yan465@gamil.com>
2022-03-14 23:02:52 +0200

    deleted:    blockchain.js 	deleted:    blockchain1.js 	deleted:    blockchain2.js 	deleted:    blockchain3.js 	deleted:    main.js 	deleted:    main1.js 	deleted:    main2.js 	deleted:    main3.js

--- main.js
+++
@@ -1,9 +0,0 @@
-const {Blockchain,Block}=require('./blockchain.js')
-
-let micaCoin=new Blockchain()
-micaCoin.addBlock(new Block(1,"2/2/2012",{amount:4}))
-micaCoin.addBlock(new Block(2, "2/2/2012", {
-    amount: 8
-}))
-
-console.log(JSON.stringify(micaCoin,null,4))
